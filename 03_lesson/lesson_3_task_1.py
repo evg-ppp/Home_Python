9d22415000f833630c6ba0c514deb0543775dba3
Евгений-старый <jankovskiyevgenij@gmail.comm>
2026-05-04 15:16:58 +0300

    добавлен файл в 1 задание 3 урока

---
+++ 03_lesson/lesson_3_task_1.py
@@ -0,0 +1,9 @@
+# Импортировать класс User
+from user import User
+# Создаем объект my_user
+my_user = User("Евгений", "Пацеля")
+
+
+my_user.print_first_name()
+my_user.print_last_name()
+my_user.print_full_name()
