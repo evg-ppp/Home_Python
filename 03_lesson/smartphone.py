306aaef262c1d0d0d50090db35b3d10e32856d77
Евгений-старый <jankovskiyevgenij@gmail.comm>
2026-05-05 17:06:45 +0300

    Добавлен файл smartphone  в задание 2 урока 3

---
+++ 03_lesson/smartphone.py
@@ -0,0 +1,6 @@
+# объявить класс
+class Smartphone:
+    def __init__(self, phone_brand, phone_model, subscriber_number=('+79…')):
+        self.phone_brand = phone_brand 
+        self.phone_model = phone_model
+        self.subscriber_number = subscriber_number
