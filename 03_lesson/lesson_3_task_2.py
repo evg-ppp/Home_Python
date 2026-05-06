9c5e1e7300c5574639f4ba5215862d8d4bd2e9ee
Евгений-старый <jankovskiyevgenij@gmail.comm>
2026-05-06 11:28:57 +0300

    Внесены изменения в  файл  задание 2 урока 3

--- 03_lesson/lesson_3_task_2.py
+++ 03_lesson/lesson_3_task_2.py
@@ -1,27 +1,20 @@
 # Импортировать класс User
 from smartphone import Smartphone
+# Объявить переменную
+catalog = []  # Создаем пустой список
+phone1 = Smartphone("Samsung", "S26", "+79100112233")
+catalog.append(phone1)
+phone2 = Smartphone("LG", "G7", "+79001001122")
+catalog.append(phone2)
+phone3 = Smartphone("Huawei", "nova 14 pro", "+79003221233")
+catalog.append(phone3)
+phone4 = Smartphone("Xiaomi", "Redmi Note 14S", "+79231002211")
+catalog.append(phone4)
+phone5 = Smartphone("Honor", "X5c", "+79252545789")
+catalog.append(phone5)
 
-
-class Smartphone:
-    def __init__(self, phone_brand, phone_model, subscriber_number=('+79…')):
-        self.phone_brand = phone_brand
-        self.phone_model = phone_model
-        self.subscriber_number = subscriber_number
-    # Объявить переменную
-    catalog = []  # Создаем пустой список
-
-    phone1 = Smartphone("Samsung", "S26", "+79100112233")
-    catalog.append(phone1)
-    phone2 = Smartphone("LG", "G7", "+79001001122")
-    catalog.append(phone2)
-    phone3 = Smartphone("Huawei", "nova 14 pro", "+79003221233")
-    catalog.append(phone3)
-    phone4 = Smartphone("Xiaomi", "Redmi Note 14S", "+79231002211")
-    catalog.append(phone4)
-    phone5 = Smartphone("Honor", "X5c", "+79252545789")
-    catalog.append(phone5)
-
-    # Наполнить список пятью разными экземплярами класса Smartphone
-    for phone in catalog:
-        print(f"{phone.phone_brand} - {phone.phone_model}"
-              f"{phone.subscriber_number}")
+# Наполнить список пятью разными экземплярами класса Smartphone
+for phone in catalog:
+    print(
+        f"{phone.phone_brand} - {phone.phone_model}. {phone.subscriber_number}"
+    )
